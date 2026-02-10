d02b68b878debf2d018ae10685f9cabf0927df1d
rickfmn <rickf@mindless.com>
2026-02-10 17:22:51 -0600

    Updated Camera Pose

--- src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -365,10 +365,10 @@ public class Vision
      * Center Camera
      */
     CENTER_CAM("CameraB",
-               new Rotation3d(0, Units.degreesToRadians(18), 0),
-               new Translation3d(Units.inchesToMeters(-4.628),
-                                 Units.inchesToMeters(-10.687),
-                                 Units.inchesToMeters(16.129)),
+               new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(-20), Units.degreesToRadians(-15)),
+               new Translation3d(Units.inchesToMeters(-9.758),
+                                 Units.inchesToMeters(-4.25),
+                                 Units.inchesToMeters(13.375)),
                VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1));
 
     /**
